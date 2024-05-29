2fea128ec8147013281119840de5996da9de3f2c
Prabhnoor Singh <72328658+PrabhnoorSingh122999@users.noreply.github.com>
2024-05-30 04:35:34 +0530

    .

---
+++ Backend/index.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
+import clinicRoutes from "./routes/clinicRoutes.js";
+import patientRoutes from "./routes/patientRoutes.js";
+import scheduleRoutes from "./routes/scheduleRoutes.js";
+import emailRoutes from "./routes/emailRoutes.js";
+import cron from "node-cron";
+import sendReminders from "./routes/sendRemindersRoute.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes with /api prefix
+app.use("/api/users", userRoutes);
+app.use("/api/clinics", clinicRoutes);
+app.use("/api/patients", patientRoutes);
+app.use("/api/schedules", scheduleRoutes);
+app.use("/api/email", emailRoutes);
+
+cron.schedule("27 17 * * *", () => {
+  sendReminders(); // Call your route for sending reminders
+});
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
